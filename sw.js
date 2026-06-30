8c7adbadc3bb1877cee7544ec73ef1487b629e9d
mattias-beachlasse <mattias@thebeach.one>
2026-06-30 22:52:54 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "spotterboard-v2";
+const CACHE = "spotterboard-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", e => {
